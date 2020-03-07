45ed3f625f64c45be97abda9d0b18f2a7ce7fe54
William Berman <WLBberman@gmail.com>
2020-03-06 20:34:29 -0800

    better printing

--- src/Untyped/Semantics.hs
+++ src/Untyped/Semantics.hs
@@ -16,14 +16,14 @@ eval1 = stopIfSame eval1'
 -- Repetition in implementation ensures consistent reduction order
 eval1' :: Term -> EvalStepResult Term
 
-eval1' (Application (Abstraction t1) (Abstraction t2)) =
-    Continue $ shift (-1) 0 $ substitute (makeReplacement 0 $ shift 1 0 $ Abstraction t2) t1
+eval1' (Application (Abstraction t1 _) (Abstraction t2 n2)) =
+    Continue $ shift (-1) 0 $ substitute (makeReplacement 0 $ shift 1 0 $ Abstraction t2 n2) t1
 
-eval1' (Application t1 (Abstraction t2)) =
-  andThen (\t1' -> Continue $ Application t1' $ Abstraction t2) (eval1 t1)
+eval1' (Application t1 (Abstraction t2 n2)) =
+  andThen (\t1' -> Continue $ Application t1' $ Abstraction t2 n2) (eval1 t1)
 
-eval1' (Application (Abstraction t1) t2) =
-  andThen (Continue . Application (Abstraction t1)) (eval1 t2)
+eval1' (Application (Abstraction t1 n1) t2) =
+  andThen (Continue . Application (Abstraction t1 n1)) (eval1 t2)
 
 eval1' (Application t1 t2) =
   andThen (\t1' -> Continue $ Application t1' t2) (eval1 t1)
--- src/Untyped/Syntax.hs
+++ src/Untyped/Syntax.hs
@@ -4,11 +4,16 @@ import Untyped.Syntax0 (Term0(..))
 import Untyped.State
 
 data Term =
-  Abstraction Term
+  Abstraction Term String
   | Application Term Term
-  | Variable DBIndex
-  deriving (Show, Eq)
+  | Variable DBIndex String
+  deriving (Eq)
 
+instance Show Term where
+  show (Abstraction term name) = "lambda " <> name <> ". " <> show term
+  show (Application (Application t11 t12) t2) = "(" <> show (Application t11 t12) <> ") " <> show t2
+  show (Application t1 t2) = show t1 <> " " <> show t2
+  show (Variable _ name) = name
 
 -- applyIndices
 
@@ -20,9 +25,9 @@ applyIndices' :: Environment -> Term0 -> (Term, Environment)
 
 applyIndices' env (Variable0 name) =
   case lookUp name env of
-    LookUpResult{local = Just localIdx} -> (Variable localIdx, env)
-    LookUpResult{global = Just globalIdx} -> (Variable globalIdx, env)
-    _ -> (Variable newGlobalIdx, env')
+    LookUpResult{local = Just localIdx} -> (Variable localIdx name, env)
+    LookUpResult{global = Just globalIdx} -> (Variable globalIdx name, env)
+    _ -> (Variable newGlobalIdx name, env')
   where
     (newGlobalIdx, env')  = addGlobal name env
 
@@ -32,7 +37,7 @@ applyIndices' env (Application0 t1 t2) =
     (t1', env') = applyIndices' env t1
     (t2', env'') = applyIndices' env' t2
 
-applyIndices' env (Abstraction0 name term) = (Abstraction term', env'')
+applyIndices' env (Abstraction0 name term) = (Abstraction term' name, env'')
   where
     (term', env') = applyIndices' (addLocal name env) term
     env'' = removeLocal name env'
@@ -47,28 +52,28 @@ makeReplacement = Replacement
 
 substitute :: Replacement -> Term -> Term
 
-substitute (Replacement idx replaceWith) (Abstraction t) =
-  Abstraction $ substitute (Replacement (idx + 1) (shift 1 0 replaceWith)) t
+substitute (Replacement idx replaceWith) (Abstraction t name) =
+  Abstraction (substitute (Replacement (idx + 1) (shift 1 0 replaceWith)) t) name
 
 substitute replacement (Application t1 t2) =
   Application (substitute replacement t1) (substitute replacement t2)
 
-substitute (Replacement idx replaceWith) (Variable replacingIdx) =
-  if idx == replacingIdx then replaceWith else Variable replacingIdx
+substitute (Replacement idx replaceWith) (Variable replacingIdx name) =
+  if idx == replacingIdx then replaceWith else Variable replacingIdx name
 
 
 -- shift
 
 shift :: DBIndex -> DBIndex -> Term -> Term
 
-shift increaseBy numBinders (Abstraction t) =
-  Abstraction $ shift increaseBy (numBinders + 1) t
+shift increaseBy numBinders (Abstraction t name) =
+  Abstraction (shift increaseBy (numBinders + 1) t) name
 
 shift increaseBy numBinders (Application t1 t2) =
   Application (shift increaseBy numBinders t1) (shift increaseBy numBinders t2)
 
-shift increaseBy numBinders (Variable idx) =
-  Variable $ if isFree numBinders idx then increaseBy + idx else idx
+shift increaseBy numBinders (Variable idx name) =
+  Variable (if isFree numBinders idx then increaseBy + idx else idx) name
 
 -- A free variable's index reaches outside the number of binders present for it
 isFree :: DBIndex -> DBIndex -> Bool
