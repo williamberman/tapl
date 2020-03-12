04aec1db356a84f47a96d467f60a3e30c438492f
William Berman <WLBberman@gmail.com>
2020-03-12 15:54:17 -0700

    loop function refactored

--- src/Lang/Repl.hs
+++ src/Lang/Repl.hs
@@ -1,44 +1,62 @@
-module Lang.Repl (loop) where
+module Lang.Repl
+  ( loop
+  ) where
 
-import System.Console.Haskeline
-import Lang.Command
-import Lang.Lang(Lang, readEvalPrint, env)
-import qualified Lang.File as File(readFile)
+import           Lang.Command
+import qualified Lang.File                as File (readFile)
+import           Lang.Lang                (Lang, env, readEvalPrint)
+import           System.Console.Haskeline
 
-import Control.Monad.IO.Class(liftIO)
+import           Control.Monad.IO.Class   (liftIO)
+import qualified Data.Foldable
 
 prompt = "> "
 
--- TODO break this function up
 loop :: Lang -> InputT IO ()
-loop repl = do
+loop lang = do
   minput <- getInputLine prompt
   case minput of
     Nothing -> return ()
     Just input ->
       case parseCommand input of
-        Left _ ->
-          case readEvalPrint repl input of
-            Left err -> do
-              outputStrLn "Error"
-              outputStrLn err
-              loop repl
-            Right (out, repl') -> do
-              outputStrLn out
-              loop repl'
+        Left _ -> do
+          lang' <- runInput lang input
+          loop lang'
         Right cmd -> do
-          case cmd of
-            Quit -> return ()
-            Env -> outputStrLn $ env repl
-            (LoadFile filename) -> do
-              contents <- liftIO $ readFile filename
-              case File.readFile repl contents of
-                Left err -> do
-                  outputStrLn "Error"
-                  outputStrLn err
-                  loop repl
-                Right (repl', Nothing) -> loop repl'
-                Right (repl', Just out) -> do
-                  outputStrLn out
-                  loop repl'
-          loop repl
+          out <- runCommand lang cmd
+          Data.Foldable.forM_ out loop
+
+runInput :: Lang -> String -> InputT IO Lang
+runInput lang input =
+  case readEvalPrint lang input of
+    Left err -> do
+      outputStrLn "Error"
+      outputStrLn err
+      return lang
+    Right (out, lang') -> do
+      outputStrLn out
+      return lang'
+
+runCommand :: Lang -> Command -> InputT IO (Maybe Lang)
+runCommand lang cmd =
+  case cmd of
+    Quit -> return Nothing
+    Env -> do
+      outputStrLn $ env lang
+      return $ Just lang
+    (LoadFile filename) -> loadFile lang filename
+
+loadFile :: Lang -> String -> InputT IO (Maybe Lang)
+loadFile lang filename = do
+  contents <- liftIO $ readFile filename
+  case File.readFile lang contents of
+    Left err -> do
+      outputStrLn "Error"
+      outputStrLn err
+      return $ Just lang
+    Right (lang', out) ->
+      case out of
+        Just out' -> do
+          outputStrLn out'
+          return $ Just lang'
+        Nothing -> return $ Just lang'
