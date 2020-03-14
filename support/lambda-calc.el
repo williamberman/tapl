5eb9e115c1f0a2a0f809725fe755f7b74532b704
William Berman <WLBberman@gmail.com>
2020-03-14 15:10:28 -0700

    comment highlighting

--- support/lambda-calc.el
+++ support/lambda-calc.el
@@ -9,7 +9,7 @@
   '("lambda"))
 
 (defconst lambda-calc--keyword-regexp
-  (regexp-opt lambda-calc-keywords 'words)
+  (regexp-opt lambda-calc--keywords 'words)
   "Regular expression for Lambda Calculus keyword highlighting.")
 
 (defconst lambda-calc--variable-name-regexp
@@ -17,19 +17,31 @@
   "Regular expression for Lambda Calculus variable name highlighting.")
 
 (defconst lambda-calc--font-lock-keywords
-  `((,lambda-calc--variable-name-regexp 0 font-lock-variable-name-face)
+  `(("\\." 0 font-lock-builtin-face)
+    (,lambda-calc--variable-name-regexp 0 font-lock-variable-name-face)
     (,lambda-calc--keyword-regexp 0 font-lock-keyword-face)))
 
 (defconst lambda-calc--font-lock-defaults
   `(,lambda-calc--font-lock-keywords)
   "Highlighting instructions for Lambda Calculus.")
 
+(defvar lambda-calc-syntax-table
+  nil
+  "Syntax table for `lambda-calc-mode'")
+
+(setq lambda-calc-syntax-table
+      (let ((table (make-syntax-table)))
+	(modify-syntax-entry ?- ". 12" table)
+        (modify-syntax-entry ?\n "> " table)
+      	table))
+
 (define-derived-mode lambda-calc-mode prog-mode "Lambda Calculus"
   "Major mode for editing Lambda Calculus."
   :group 'lambda-calc
-  (setq-local font-lock-defaults lambda-calc-font-lock-defaults)
-  (setq-local comment-start "-- ")
-  (setq-local comment-end ""))
+  (setq-local font-lock-defaults lambda-calc--font-lock-defaults)
+  (setq-local comment-start "--")
+  (setq-local comment-end "")
+  (set-syntax-table lambda-calc-syntax-table))
 
 (add-to-list 'auto-mode-alist '("\\.untyped\\'" . lambda-calc-mode))
 
