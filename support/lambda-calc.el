44c46cdabd53ba56328b4fef3310abfe0c95b241
William Berman <WLBberman@gmail.com>
2020-03-14 03:10:24 -0700

    fixed highlighting bugs in lambda-calc-mode

--- support/lambda-calc.el
+++ support/lambda-calc.el
@@ -1,3 +1,5 @@
+;;; lamda-calc.el --- A major mode for editing Lambda Calculus files  -*- lexical-binding: t; -*-
+
 (defgroup lambda-calc '()
   "Lambda Calculus"
   :group 'languages
@@ -6,11 +8,6 @@
 (defface lambda-calc-keyword-face
   '((t (:inherit font-lock-keyword-face)))
   "How to highlight Lambda Calculus keywords."
-  :group 'lambda-calc)
-
-(defface lambda-calc-operator-face
-  '((t (:inherit font-lock-keyword-face)))
-  "How to highlight Lambda Calculus operators."
   :group 'lambda-calc)
 
 (defface lambda-calc-variable-name-face
@@ -25,31 +22,25 @@
   (regexp-opt lambda-calc-keywords 'words)
   "Regular expression for Lambda Calculus keyword highlighting.")
 
-(defconst lambda-calc-operators
-  '("="))
-
-(defvar lambda-calc--operator-regexp
-  (regexp-opt lambda-calc-operators 'words)
-  "Regular expression for Lambda Calculus operator highlighting.")
-
-;; TODO this is not working
 (defvar lambda-calc--variable-name-regexp
   "^\\w+"
   "Regular expression for Lambda Calculus variable name highlighting.")
 
 (defconst lambda-calc--font-lock-keywords
-  `((,lambda-calc--keyword-regexp lambda-calc-keyword-face)
-    (,lambda-calc--operator-regexp lambda-calc-operator-face)
-    (,lambda-calc--variable-name-regexp lambda-calc-variable-name-face)))
+  `((,lambda-calc--variable-name-regexp 0 font-lock-variable-name-face)
+    (,lambda-calc--keyword-regexp 0 font-lock-keyword-face)))
 
 (defvar lambda-calc-font-lock-defaults
-  `(,lambda-calc--font-lock-keywords))
+  `(,lambda-calc--font-lock-keywords)
+  "Highlighting instructions for Lambda Calculus.")
 
 (define-derived-mode lambda-calc-mode prog-mode "Lambda Calculus"
   "Major mode for editing Lambda Calculus."
   :group 'lambda-calc
-  (setq-local font-lock-defaults lambda-calc--font-lock-keywords)
+  (setq-local font-lock-defaults lambda-calc-font-lock-defaults)
   (setq-local comment-start "-- ")
   (setq-local comment-end ""))
 
 (add-to-list 'auto-mode-alist '("\\.untyped\\'" . lambda-calc-mode))
+
+(provide 'lambda-calc)
